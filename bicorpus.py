4ff97f8811a78cd8adbc43b0fbc1eb0df915d527
Attila Zseder <zseder@ilab.sztaki.hu>
2012-01-13 16:49:09 +0100

    Bicorpus class: - two interesting cache instead of one: both directions - based on former, set searching now can also be bidirectional   (sets are only at one side at once)

--- bicorpus.py
+++ bicorpus.py
@@ -30,26 +30,32 @@ class BiCorpus:
             del self._coocc_cache
         if hasattr(self, "interesting"):
             del self.interesting
-        self._coocc_cache = defaultdict(int)
-        self.interesting = defaultdict(dict)
+        self._coocc_cache = (defaultdict(int), defaultdict(int))
+        self.interesting = (defaultdict(dict), defaultdict(dict))
 
     def build_cache(self):
         logging.info("Buildind cache...")
+        gc.disable()
         self.create_cache()
         for sen_i in xrange(len(self._src)):
             if sen_i * 100 / len(self._src) > (sen_i - 1) * 100 / len(self._src):
                 logging.debug("{0}% done".format(sen_i * 100/len(self._src)))
             self.add_sentence_pair_to_cache(self._src[sen_i], self._tgt[sen_i])
         self.filter_interesting_pairs()
+        gc.enable()
         logging.info("Buildind cache done")
 
     def add_sentence_pair_to_cache(self, src, tgt):
         for src_tok in src:
             for tgt_tok in tgt:
                 try:
-                    self.interesting[src_tok][tgt_tok] += 1
+                    self.interesting[0][src_tok][tgt_tok] += 1
                 except KeyError:
-                    self.interesting[src_tok][tgt_tok] = 1
+                    self.interesting[0][src_tok][tgt_tok] = 1
+                try:
+                    self.interesting[1][tgt_tok][src_tok] += 1
+                except KeyError:
+                    self.interesting[1][tgt_tok][src_tok] = 1
 
     def filter_interesting_pairs(self, max_per_word=10):
         logging.info("Filtering interesting pairs...")
@@ -59,8 +65,10 @@ class BiCorpus:
         if hasattr(self, "_coocc_cache"):
             del self._coocc_cache
 
-        for src in self.interesting:
-            self.interesting[src] = dict(sorted(self.interesting[src].iteritems(), key=lambda x: x[1], reverse=True)[:max_per_word])
+        for src in self.interesting[0]:
+            self.interesting[0][src] = dict(sorted(self.interesting[0][src].iteritems(), key=lambda x: x[1], reverse=True)[:max_per_word])
+        for tgt in self.interesting[1]:
+            self.interesting[1][tgt] = dict(sorted(self.interesting[1][tgt].iteritems(), key=lambda x: x[1], reverse=True)[:max_per_word])
         logging.info("Filtering interesting pairs done")
 
     def ngram_pair_context(self, pair, max_len=None):
@@ -120,14 +128,6 @@ class BiCorpus:
                 src_ngram_to_remove[src] |= indices
                 tgt_ngram_to_remove[tgt] |= indices
 
-            # remove all occurences from @interesting cache
-            for src_tok in src:
-                for tgt_tok in tgt:
-                    try:
-                        self.interesting[src_tok][tgt_tok] -= len(indices)
-                    except KeyError:
-                        pass
-
         for ngram in src_ngram_to_remove:
             indices = src_ngram_to_remove[ngram]
             self._src.remove_ngram(ngram, indices, self._backup)
@@ -143,9 +143,9 @@ class BiCorpus:
         logging.info("String difference phase started")
         src_index = self._src._index
         tgt_index = self._tgt._index
-        for src in self.interesting:
+        for src in self.interesting[0]:
             src_tok = self._src.ints_to_tokens([src])[0].lower()
-            for tgt, _ in self.interesting[src].iteritems():
+            for tgt, _ in self.interesting[0][src].iteritems():
                 tgt_tok = self._tgt.ints_to_tokens([tgt])[0].lower()
                 ratio = float(len(src_index[src])) / len(tgt_index[tgt])
                 if ratio > 3 or ratio < 1/3.0:
@@ -172,18 +172,32 @@ class BiCorpus:
 
         logging.info("String difference phase done")
 
-    def generate_unigram_pairs(self, min_coocc=1, max_coocc=None):
-        for result in self.generate_unigram_set_pairs(min_coocc, max_coocc, max_len=1):
-            src_ngram_set, results_for_src = result
-            src_ngram = src_ngram_set.pop()
-            for result_for_src in results_for_src:
-                tgt_ngram_set, cont_table = result_for_src
+    def __generate_unigram_pairs(self, min_coocc, max_coocc):
+        # we don't need to run generate_unigram_set_pairs() in both ways,
+        # because when searching for 1-1 unigrams, table will be symmetric
+        for result_for_one_ngram in self.generate_unigram_set_pairs(min_coocc, max_coocc, max_len=1, both_ways=True):
+            # when running generate_unigram_set_pairs(), results are
+            # collapsed by one src (or tgt in reverse mode) token for
+            # later usage so here we need to handle them one by one
+            for result in result_for_one_ngram:
+                src_ngram_set, tgt_ngram_set, table = result
+                src_ngram = src_ngram_set.pop()
                 tgt_ngram = tgt_ngram_set.pop()
-                yield ((src_ngram, tgt_ngram), cont_table)
+                yield ((src_ngram, tgt_ngram), table)
 
-    def generate_unigram_set_pairs(self, min_coocc=1, max_coocc=None, min_len=1, max_len=3):
-        src_index = self._src._index
-        tgt_index = self._tgt._index
+    def generate_unigram_pairs(self, min_coocc=1, max_coocc=None):
+        for _ in self.__generate_unigram_pairs(min_coocc, max_coocc):
+            yield _
+
+    def __generate_unigram_set_pairs(self, min_coocc=1, max_coocc=None, min_len=1, max_len=3, reverse=False):
+        if reverse is False:
+            src_index = self._src._index
+            tgt_index = self._tgt._index
+            interesting = self.interesting[0]
+        else:
+            src_index = self._tgt._index
+            tgt_index = self._src._index
+            interesting = self.interesting[1]
         gc.disable()
         src_len = len(src_index)
         for i, src_tok in enumerate(src_index):
@@ -192,7 +206,7 @@ class BiCorpus:
 
             src_occ = src_index[src_tok] 
 
-            possible_tgts = self.interesting[src_tok].items()
+            possible_tgts = interesting[src_tok].items()
             sum_ = sum((x[1] for x in possible_tgts))
             sorted_possible_tgts = sorted((x for x in possible_tgts if x[1] >= sum_ / 10), key=lambda x: x[1], reverse=True)[:max_len+2]
 
@@ -214,7 +228,9 @@ class BiCorpus:
                             tgt_set = tgt_index[tgt_tok]
                             prev_len = len(tgt_occ)
                             tgt_occ |= tgt_set
-                            if len(tgt_occ) - prev_len <= 1:
+                            # if gain is too small, there is no point in continuing
+                            # with this set
+                            if len(tgt_occ) - prev_len <= len(src_occ) / 20:
                                 gain_for_every_word = False
                                 break
                         if not gain_for_every_word:
@@ -225,11 +241,21 @@ class BiCorpus:
                     coocc = len(src_occ & tgt_occ)
                     if (coocc >= min_coocc and (max_coocc is None or coocc <= max_coocc)):
                         cont_table = self.contingency_table(None, src_occ_s=src_occ, tgt_occ_s=tgt_occ, coocc_c=coocc)
-                        results.append( (set([(tgt_tok[0],) for tgt_tok in tgt_toks]), cont_table) )
+                        if reverse is False:
+                            results.append( (set([(src_tok,)]), set([(tgt_tok[0],) for tgt_tok in tgt_toks]), cont_table) )
+                        else:
+                            results.append( (set([(tgt_tok[0],) for tgt_tok in tgt_toks]), set([(src_tok,)]), cont_table) )
                     else:
                         break
-            yield set([(src_tok,)]), results
+            yield results
         gc.enable()
+
+    def generate_unigram_set_pairs(self, min_coocc=1, max_coocc=None, min_len=1, max_len=3, both_ways=True):
+        for _ in self.__generate_unigram_set_pairs(min_coocc, max_coocc, min_len, max_len, False):
+            yield _
+        if both_ways:
+            for _ in self.__generate_unigram_set_pairs(min_coocc, max_coocc, min_len, max_len, True):
+                yield _
 
     def ngram_pair_neighbours(self, pair, ngram_indices=None, max_len=4):
         src, tgt = pair
